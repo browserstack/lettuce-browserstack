599abb47ab175e73e90177bce1537bee206aa5b7
Vibhaj Rajan <vibhaj8@gmail.com>
2016-07-01 17:38:38 +0530

    add server in config

--- features/terrain.py
+++ features/terrain.py
@@ -43,7 +43,7 @@ def setup_browser(feature):
 
     world.browser = webdriver.Remote(
         desired_capabilities=desired_capabilities,
-        command_executor="http://%s:%s@hub.browserstack.com/wd/hub" % (BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY)
+        command_executor="http://%s:%s@%s/wd/hub" % (BROWSERSTACK_USERNAME, BROWSERSTACK_ACCESS_KEY, CONFIG['server'])
     )
 
 @after.each_feature
